58a31230014de17e90c94894372bb4ad6faf1b29
KaterunaBatyuk <katerina.batuk89@gmail.com>
2022-10-18 01:57:19 +0300

    Перше завдання

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,31 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryRef = document.querySelector('.gallery');
+const galleryMarkUp = createMarkUp(galleryItems);
+
+galleryRef.innerHTML = galleryMarkUp;
+galleryRef.addEventListener('click', onImageClick);
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+function createMarkUp(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join('');
+}
+
+function onImageClick(event) {
+  event.preventDefault();
+}
 console.log(galleryItems);
