2c4fc5fe8b718eaa56b494e54b540b7ca99549ee
KaterunaBatyuk <katerina.batuk89@gmail.com>
2022-10-19 23:23:11 +0300

    2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,40 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframeRef = document.querySelector('#vimeo-player');
+const player = new Player(iframeRef);
+
+const CURRENT_TIME = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(savedTimeToUpdate, 1000));
+
+updatedTiming();
+
+function savedTimeToUpdate(data) {
+  localStorage.setItem(CURRENT_TIME, JSON.stringify(data));
+}
+
+function updatedTiming() {
+  const savingTime = localStorage.getItem(CURRENT_TIME);
+
+  if (!savingTime) {
+    return;
+  }
+
+  const savingTimeInSeconds = JSON.parse(
+    localStorage.getItem(CURRENT_TIME)
+  ).seconds;
+
+  player
+    .setCurrentTime(savingTimeInSeconds)
+    .then(function () {})
+    .catch(function (error) {
+      switch (error.name) {
+        case 'RangeError':
+          break;
+
+        default:
+          break;
+      }
+    });
+}
