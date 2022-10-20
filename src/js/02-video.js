72b2411b5464d21402b0c1f3a64cefbc54606324
KaterunaBatyuk <katerina.batuk89@gmail.com>
2022-10-20 18:50:17 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,37 +4,11 @@ import throttle from 'lodash.throttle';
 const iframeRef = document.querySelector('#vimeo-player');
 const player = new Player(iframeRef);
 
-const CURRENT_TIME = 'videoplayer-current-time';
-
-player.on('timeupdate', throttle(savedTimeToUpdate, 1000));
-
-updatedTiming();
-
-function savedTimeToUpdate(data) {
-  localStorage.setItem(CURRENT_TIME, JSON.stringify(data));
+player.on('timeupdate', throttle(getCurrentTime, 1000));
+function getCurrentTime({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
 }
-
-function updatedTiming() {
-  const savingTime = localStorage.getItem(CURRENT_TIME);
-
-  if (!savingTime) {
-    return;
-  }
-
-  const savingTimeInSeconds = JSON.parse(
-    localStorage.getItem(CURRENT_TIME)
-  ).seconds;
-
-  player
-    .setCurrentTime(savingTimeInSeconds)
-    .then(function () {})
-    .catch(function (error) {
-      switch (error.name) {
-        case 'RangeError':
-          break;
-
-        default:
-          break;
-      }
-    });
+if (localStorage.getItem('videoplayer-current-time')) {
+  let currentSec = localStorage.getItem('videoplayer-current-time');
+  player.setCurrentTime(currentSec);
 }
